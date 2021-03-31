a97c3b67ef932a7e4185266b3688d303470bb926
TJ Taylor <dugancathal@gmail.com>
2021-03-30 21:45:54 -0600

    Fix ParameterName to ParameterKey in aws create-stack

--- .circleci/deploy-stack.sh
+++ .circleci/deploy-stack.sh
@@ -6,5 +6,5 @@ DEPLOY_TAG=$2
 aws cloudformation create-stack \
   --stack-name=$STACK_NAME \
   --template-body=file://infra/servers.yml \
-  --parameters ParameterName=DeployTag,ParameterValue=$2
+  --parameters ParameterKey=DeployTag,ParameterValue=$2
 aws cloudformation wait stack-create-complete --stack-name=$STACK_NAME
