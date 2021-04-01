4219c7fb3203c0bb80467a1bb106f606175c2618
TJ Taylor <dugancathal@gmail.com>
2021-04-01 15:19:26 -0600

    Ensure we look up the previous environment correctly

--- .circleci/determine-env-color.sh
+++ .circleci/determine-env-color.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-STACK_NAME=$1
+CURRENT_STACK_NAME=$1
+PREVIOUS_STACK_NAME="$(aws cloudformation describe-stacks \
+  --query "Stacks[?Parameters[?ParameterKey == 'EnvColor'] && StackName != '${CURRENT_STACK_NAME}'].StackName" \
+  --output text \
+)"
+
+NUM_STACKS="$(echo "${PREVIOUS_STACK_NAME}" | sed '/^[[:space:]]*$/d' | wc -l | tr -d ' ' )"
+
+if [[ "$NUM_STACKS" == '0' ]]; then
+  echo "No stacks found matching criteria" >&2
+  exit 0
+elif [[ "$NUM_STACKS" > '1' ]]; then
+  echo "Unable to delete stack!!! Multiple stacks found matching criteria" >&2
+  echo "$PREVIOUS_STACK_NAME" >2
+  exit 1
+fi
 
 current_color="$(aws cloudformation describe-stacks \
-  --stack-name="$STACK_NAME" \
+  --stack-name="${PREVIOUS_STACK_NAME}" \
   --query "Stacks[0].Parameters[?ParameterKey == 'EnvColor'].ParameterValue" \
   --output text)"
 
